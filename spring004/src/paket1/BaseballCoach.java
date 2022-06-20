92b405ce6883d3ed5f0a75857edf49e136c64792
Ali <rokel_02@hotmail.com>
2022-06-21 02:22:34 +0300

    Creating bean with annotations

--- spring004/src/paket1/BaseballCoach.java
+++ spring004/src/paket1/BaseballCoach.java
@@ -1,11 +1,12 @@
 package paket1;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BaseballCoach implements Coach {
-
+	
 	@Autowired
 	private FortuneService fortuneService;//field injection --> no need setter method or constructor
 	
